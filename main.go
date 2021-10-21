6fc1e70568bb5199f1b519b69c2972c1a705709b
rajesha.puttaramaiah@oracle.com <rajesha.puttaramaiah@oracle.com>
2021-10-21 04:12:25 +0000

    Generate unique ids

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
 	"fmt"
 	"reflect"
+	"strconv"
+	"time"
 
 	"github.com/iRajesha/experiments/src/panic"
 )
@@ -83,6 +88,78 @@ backToFor:
 	fmt.Printf("Concatinated value is %v", str1)
 
 	extractInterfaceType(ray)
+
+	checkMarshalling()
+	fmt.Printf("\n\n\n\n\n\n\n")
+	gernateBusinessObjectId()
+
+}
+
+func gernateBusinessObjectId() {
+	var channelId = "0"
+	now := time.Now()
+	year := now.Year()
+	lastTwoDigitsOfYear := year % 1e2
+	_, week := now.UTC().ISOWeek()
+
+	businessId := channelId + strconv.Itoa(lastTwoDigitsOfYear) + strconv.Itoa(week) + fmt.Sprintf("%d", get13MiddleDigits("4b537cebb9f67d9bcfe29d38d03d76febe54808a16238815e5ad28b695c197b7"))
+
+	fmt.Printf("Last two digits --> %v\n", lastTwoDigitsOfYear)
+	fmt.Printf("Final Business Identifier %v\n", businessId)
+}
+
+func get13MiddleDigits(txId string) int64 {
+	//hasher := sha256.New()
+
+	bv := []byte(txId)
+	result := sha256.Sum256(bv)
+	hexString := hex.EncodeToString(result[:])
+	last13HexDigits := hexString[50 : len(hexString)-1]
+	fmt.Printf(" last13HexDigits ->  %v\n", last13HexDigits)
+	decimal, err := strconv.ParseInt(last13HexDigits, 16, 64)
+	if err != nil {
+		fmt.Printf("Error %v\n ", err)
+	}
+	last13digits := decimal % 1e13
+	fmt.Printf("random 13 digits %v\n", last13digits)
+
+	return last13digits
+}
+
+func checkMarshalling() {
+	var walletRespPayload = struct {
+		MspId  string `json:"mspId"`
+		Status string `json:"status"`
+	}{}
+
+	type walletRespPayloadStruct struct {
+		Username string `json:"username"`
+		Secret   string `json:"secret"`
+		MspId    string `json:"mspId"`
+		Status   string `json:"status"`
+	}
+	structVar := walletRespPayloadStruct{}
+	//payload := "{\"username\":\"NewOrgTMAId\",\"secret\":\"63c1dd951ffedf6f7fd968ad4efa39b8ed584f162f46e715114ee184f8de9201\",\"mspId\":\"TMAFounderTEST\",\"status\":\"TMA Member\"}"
+	payload := string(`{"username":"NewOrgTMAId","secret":"63c1dd951ffedf6f7fd968ad4efa39b8ed584f162f46e715114ee184f8de9201","mspId":"TMAFounderTEST","status":"TMA Member"}`)
+	bPayload, _ := json.Marshal(payload)
+
+	fmt.Printf("Marshalled payload --> %v\n", string(bPayload))
+	json.Unmarshal([]byte(payload), &walletRespPayload)
+	fmt.Printf("Unmashalled Payload %v\n", walletRespPayload)
+	json.Unmarshal([]byte(payload), &structVar)
+	fmt.Printf("Unmashalled Payload %v\n", structVar)
+
+	//value := string(`{"TMAFounderTEST":"Hello"}`)
+	value, err := json.Marshal("TMAFounderTEST")
+	if err != nil {
+		fmt.Printf("Error %v\n", err)
+	}
+	fmt.Printf("Value -- %v\n", string(value))
+	var obj interface{}
+	err = json.Unmarshal([]byte(value), &obj)
+	if err != nil {
+		fmt.Printf("Error %v\n", err)
+	}
 
 }
 
